2ade5bbe3a13bd2b4e3155acd891aa9eb9252980
RAKwireless <RAKWireless@github.com>
2019-06-18 11:24:33 +0800

    fix script error

--- sysconf/set_ip.sh
+++ sysconf/set_ip.sh
@@ -3,11 +3,9 @@
 # Stop on the first sign of trouble
 #set -e
 
-SCRIPT_COMMON_FILE=$(pwd)/../rak/rak/shell_script/rak_common.sh
-source $SCRIPT_COMMON_FILE
 
 if [ $UID != 0 ]; then
-    echo_error "Operation not permitted. Forgot sudo?"
+    echo "Operation not permitted. Forgot sudo?"
     exit 1
 fi
 
